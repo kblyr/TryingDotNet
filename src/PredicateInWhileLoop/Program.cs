7e5eb81f5e589fac9a6354efd5593f21c30539ae
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-10-10 13:57:15 +0800

    PredicateInWhileLoop

---
+++ src/PredicateInWhileLoop/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Hello World");
+var x = 0;
+
+new Looper().While(MyPredicate);
+
+
+bool MyPredicate()
+{
+    return x++ < 10;
+}
+
+class Looper
+{
+    public void While(Func<bool> predicate)
+    {
+        while(predicate())
+        {
+            Console.WriteLine("");
+        }
+    }
+}
