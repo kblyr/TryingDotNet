670878a53f9cdcc672a29857de0bddc7332e54dc
John Josua Paderon <johnjosua.paderon@outlook.com>
2021-11-25 14:05:31 +0800

    MassTransitSample

---
+++ src/MassTransit/MassTransitSample.ConsoleApp/Program.cs
@@ -0,0 +1,53 @@
+﻿using MassTransit;
+using MassTransitSample.ConsoleApp;
+
+var busControl = Bus.Factory.CreateUsingInMemory(config => {
+    config.ReceiveEndpoint(endpoint => {
+        endpoint.Consumer<ValueEnteredConsumer>();
+    });
+});
+
+await busControl.StartAsync();
+
+try
+{
+    Console.WriteLine(typeof(ValueEntered).FullName);
+
+    while (true) {
+        var value = await Task.Run(() => {
+            Console.WriteLine("Enter message (or quit to exit)");
+            Console.Write("> ");
+            return Console.ReadLine();
+        });
+
+        if (value is null)
+            continue;
+
+        if ("quit".Equals(value, StringComparison.OrdinalIgnoreCase))
+            break;
+
+        await busControl.Publish<ValueEntered>(new {
+            Value = value ?? ""
+        });
+    }
+}
+finally
+{
+    await busControl.StopAsync();
+}
+
+namespace MassTransitSample.ConsoleApp
+{
+    public interface ValueEntered
+    {
+        string Value { get; }
+    }
+
+    class ValueEnteredConsumer : IConsumer<ValueEntered>
+    {
+        public async Task Consume(ConsumeContext<ValueEntered> context)
+        {
+            Console.WriteLine("Value: {0}", context.Message.Value);
+        }
+    }
+}
